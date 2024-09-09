8d046be03aa426f133b84920ff886c7e735357fd
Salty876 <100157151+Salty876@users.noreply.github.com>
2024-09-08 20:41:36 -0600

    Almost finished nav bar and started the login screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,12 @@
 <script>
 
     import {currentUser, pb} from '../lib/pocketbase'
-
-
+    import { logout } from '../lib/authFuncs'
+ 
     let searchValue
     let searchBarStatus = false
+
+
 
     
 
@@ -19,7 +21,7 @@
     <div class="leftSide" >
 
     <!-- Where the sandwich menu goes -->
-    <h3>MangaTensou</h3>
+    <h1>MangaTenshou</h1>
     <!-- Where the logo goes -->
     </div>
 
@@ -28,17 +30,21 @@
             <input
                 class="searchbar"
                 type="text"
+                placeholder="Search"
                 bind:value={searchValue}
           
             >
-            <button>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+            <button class='searchButton'>
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="white"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                    <path d="M21 21l-6 -6"/>
+                </svg>            
             </button>
         </div>
 
         {#if $currentUser}
             <div class="dropdown">
-                <a href="/">
+                <a href="/user/profile">
                     <img class='profilePicture' src="http://127.0.0.1:8090/api/files/users/{$currentUser.id}/{$currentUser.avatar}?thumb=50x50" alt="pfp">
                 </a>
                 <div class='dropdownContent'>
@@ -51,7 +57,7 @@
 
                    <ul>
                         <li>
-                            <a href="/user/profile">profile</a>
+                            <a href="/user/profile">Profile</a>
                         </li>
 
                         <li>
@@ -61,9 +67,66 @@
                         <li>
                             <a href="/user/settings">Settings</a>
                         </li>
+                        <hr>
+                        <button class="logOut" on:click={logout}>
+                            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-logout-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+                                <path d="M15 12h-12l3 -3" />
+                                <path d="M6 15l-3 -3" />
+                            </svg>
+                            Sign Out
+                           </button>
                    </ul>
+                
+              
                 </div>
             </div>
+
+        {:else}
+
+        <div class="dropdown">
+            <a href="/">
+                <img class='profilePicture' src="https://placehold.co/50" alt="pfp">
+            </a>
+            <div class='dropdownContent' id="guest">
+               <div class="nameAndPfp">
+                <img class='profilePicture' src="https://placehold.co/50" alt="guest pfp">
+                <h3>Guest</h3>
+               </div>
+
+               <hr>
+
+            
+                  <div class="buttonBox">
+                    <a href="/login" class="authLinks" data-sveltekit-reload>
+                        <button class="logOrRegister" >
+                            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-logout-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+                                <path d="M15 12h-12l3 -3" />
+                                <path d="M6 15l-3 -3" />
+                            </svg>
+                            Log In
+                        </button>
+                    </a>
+
+                       <a href="/register" class="authLinks" data-sveltekit-reload>
+                        <button class="logOrRegister" id='register' >
+                            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-logout-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+                                <path d="M15 12h-12l3 -3" />
+                                <path d="M6 15l-3 -3" />
+                            </svg>
+                            Register
+                        </button>
+                        </a>
+
+                    </div>
+    
+            
+          
+            </div>
+        </div>
+            
         {/if}
     </div>
 
@@ -71,11 +134,18 @@
 
 
 
-<slot></slot>
+<slot class='slot'></slot>
+
+{#if $currentUser}
+    <h1>logged</h1>
+{:else}
+    <h1>Guest</h1>
+{/if}
 
 <style>
     :global(body){
         margin: 0;
+        background-color: #09090B;
 
         }
     nav{
@@ -95,7 +165,7 @@
 
     }
 
-    button{
+    .searchButton{
         width: 2vw;
         background-color: #71717a;
         border: none;
@@ -142,9 +212,18 @@
         border-radius: 20px;
         padding-left: 10px;
 
+        color: white;
+        font-weight: 600;
+
 
         
         }
+
+    ::placeholder{
+        color: white;
+        opacity: 1;
+        font-weight: 600;
+    }
 
 
 
@@ -169,10 +248,12 @@
     }
 
     .dropdownContent{
+        padding-top: 30px;
         display: none;
         min-width: 20vw;
         height: 50vh;
         background-color: #262626;
+
     
     }
 
@@ -185,8 +266,138 @@
 
     }
 
+    .dropdown:hover :global(body){
+        filter: blur(10px);
+    }
+
 
     hr{
         width: 90%;
+        padding: 0;
+    }
+
+    ul{
+        width: 100%;
+        height: 50%;
+        padding: 0;
+
+       
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h3{
+        color: white;
+        font-weight: 700;
+    }
+
+    a{
+        text-decoration: none;
+        color: white;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .authLinks{
+        width: 100%;
+        height: 50%;
+    }
+
+    li{
+        width: 100%;
+        height: 20%;
+        padding: 0;
+        text-decoration: none;
+        list-style: none;
+  
+
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+
+
+    hr{
+        color: white;
+    }
+    li:hover{
+        background-color: #9333ea;
+    }
+
+    .logOut{
+        width: 90%;
+        height: 30%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 1vw;
+
+        border-color:#9333ea;
+        border-style: solid;
+        border-radius: 6px;
+
+        background-color: #9333ea;
+
+        font-size: 1.25rem;
+        color: white;
+        font-weight: 700;
+    }
+
+    #guest{
+        
+        justify-content: space-between;
+        gap: 10%;
+     
+
+        
+
+    }
+
+    .buttonBox{
+        width: 100%;
+        height: 70%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        gap: 2%;
+
+    }
+
+    .logOrRegister{
+        width: 90%;
+        height: 50%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 1vw;
+
+        border-color:#9333ea;
+        border-style: solid;
+        border-radius: 6px;
+
+        background-color: #9333ea;
+
+        font-size: 1.25rem;
+        color: white;
+        font-weight: 700;
+    }
+
+    #register{
+        background-color: #262626;
+        border: none;
+
+    }
+
+    div:hover .dropdownContent:not(:hover){
+        filter: blur(50%);
     }
 </style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,46 +2,122 @@
         import {currentUser, pb} from '../../lib/pocketbase'
         let username
         let password
+        let message
 
         async function login(){
-        await pb.collection('users').authWithPassword(username,password)
+        try{
+            const logIn = await pb.collection('users').authWithPassword(username,password)
+            message = ''
+            window.location.href = "/"
+        }
+        catch {
+            message = 'Username or Password is incorrect'
+            console.log(message)
+      
+            
+        }
     }
+        
+    
 
-    function logout(){
-        pb.authStore.clear()
-    }
 
 </script>
 
-
+<div class='mainFrame'>
 {#if $currentUser}
 
-    <p>Already signed in as {$currentUser.username}</p>
+    <h1 class="alrDone">Already signed in</h1>
     
 
 {:else}
 
 <form>
+    <h1>Log In</h1>
     <input
-    placeholder="username"
+    placeholder="Username"
     type="text"
     bind:value={username}
     >
     <input
-    placeholder="password"
-    type="text"
+    placeholder="Password"
+    type="password"
     bind:value={password}
     >
 
     <button on:click={login}>
         login
     </button>
+    {#if message}
+    <h2>{message}</h2>
+    {/if}
 </form>
+
 {/if}
 
-<button on:click={logout}>
-    logout
-</button>
+</div>
 
 
+<style>
+    .mainFrame{
+        width: 100vw;
+        height: 80vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: baseline;
+    }
 
+    form{
+        width: 40%;
+        height: 70%;
+        display: flex;
+        flex-direction: column;
+        /* align-items: center; */
+        gap: 5%;
+    }
+
+    h1{
+        color: white;
+    }
+
+    h2{
+        color: white;
+    }
+
+    input{
+        height: 9%;
+        background-color: #71717a;
+        border: none;
+        border-radius: 3px;
+        padding-left: 10px;
+
+
+        color: white;
+        font-size: 1.25rem;
+
+    }
+
+    input:focus{
+        
+        outline-style: solid;
+        outline-width: 0.5vh;
+        outline-color: #9333ea;
+    }
+
+
+    button{
+        height: 10%;
+        font-size: 1.5rem;
+        color: white;
+        background-color: #9333ea;
+        border: none;
+    }
+    button:hover{
+       
+        color:#9333ea;
+        background-color: #09090B;
+        outline-style: solid;
+        outline-width: 0.5vh;
+        outline-color: #9333ea;
+    }
+</style>
